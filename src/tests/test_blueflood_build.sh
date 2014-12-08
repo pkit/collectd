70244a9d9577a811ca2f93ddb73dbe9df0187156
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2014-12-08 21:05:49 +0200

    treat warnings like errors for test build

--- src/tests/test_blueflood_build.sh
+++ src/tests/test_blueflood_build.sh
@@ -1,4 +1,4 @@
-CFLAGS="-I./src -I./src/daemon -D HAVE_CONFIG_H -g -O0 -Werror"
+CFLAGS="-I./src -I./src/daemon -D HAVE_CONFIG_H -g -O0 -Werror -Wall"
 rm test_blueflood ./src/tests/test_blueflood.o -f
 gcc -c ./src/tests/mock/plugin.c -o ./src/tests/mock/plugin.o $CFLAGS
 gcc -c ./src/tests/test_blueflood.c -o ./src/tests/test_blueflood.o $CFLAGS
